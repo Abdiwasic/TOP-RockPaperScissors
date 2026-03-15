2cfaddedfb1b6548510618bbad774bd181e22df2
Abdiwasic <93664874+Abdiwasic@users.noreply.github.com>
2026-03-15 23:09:35 +0300

    made UI for the game instead of the console/prompt

--- script.js
+++ script.js
@@ -1,61 +1,65 @@
+const bodyEl = document.querySelector("body");
+const btns = document.querySelectorAll(".btn");
+const result = document.querySelector(".result");
+const computerScoreSpan = document.querySelector(".computer-score");
+const playerScoreSpan = document.querySelector(".player-score");
+const gameResult = document.querySelector(".game-result");
+
 function getComputerChoice() {
-    const random = Math.floor(Math.random() * 3) + 1;
-    
-    switch (random) {
-        case 1:
-            return "rock"
-            break;
-        case 2:
-            return "paper";
-            break;
-        case 3: 
-            return "scissors";
-            break;
-    }
+  const random = Math.floor(Math.random() * 3) + 1;
+  switch (random) {
+    case 1:
+      return "rock";
+      break;
+    case 2:
+      return "paper";
+      break;
+    case 3:
+      return "scissors";
+      break;
+  }
 }
-
-function getHumanChoice() {
-    const userInput = prompt("What are you choosing: rock, paper or Scissors");
-    return userInput;
-}
-
 let humanScore = 0;
 let computerScore = 0;
 
 function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        const caseInsHumanChoice = humanChoice
-        if (caseInsHumanChoice === "rock" && computerChoice === "paper" ||
-            caseInsHumanChoice === "paper" && computerChoice === "scissors" ||
-            caseInsHumanChoice === "scissors" && computerChoice === "rock" ) {
-            console.log(`You lose! ${computerChoice} beats ${caseInsHumanChoice}`);
-            computerScore += 1;
-        } else if (
-            computerChoice === "rock" && caseInsHumanChoice === "paper" ||
-            computerChoice === "paper" && caseInsHumanChoice === "scissors" ||
-            computerChoice === "scissors" && caseInsHumanChoice === "rock"
-        ) {
-            console.log(`You win! ${caseInsHumanChoice} beats ${computerChoice}`);
-            humanScore += 1;
-        } else {
-            console.log(`Draw! ${caseInsHumanChoice} and ${computerChoice}`);
-        }
-    
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice === "rock" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "rock")
+    ) {
+      result.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+      computerScore += 1;
+      computerScoreSpan.textContent = computerScore;
+    } else if (
+      (computerChoice === "rock" && humanChoice === "paper") ||
+      (computerChoice === "paper" && humanChoice === "scissors") ||
+      (computerChoice === "scissors" && humanChoice === "rock")
+    ) {
+      result.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+      humanScore += 1;
+      playerScoreSpan.textContent = humanScore;
+    } else {
+      result.textContent = `Draw! ${humanChoice} and ${computerChoice}`;
     }
 
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+    if (computerScore === 5) {
+      btns.forEach((btn) => btn.remove());
+      bodyEl.removeChild(result);
+      gameResult.textContent = `Game Over! Computer wins the game`;
+    } else if (humanScore === 5) {
+      btns.forEach((btn) => btn.remove());
+      bodyEl.removeChild(result);
+      gameResult.textContent = `Game Over! player wins the game`;
+    }
+  }
 
-    playRound(humanSelection, computerSelection);
-}
-for (let i = 1; i <= 5; i++) {
-    playGame();
+  btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      playRound(e.target.id, getComputerChoice());
+    });
+  });
 }
 
-if (humanScore > computerScore) {
-    console.log(`Game Over: Player wins the game with \n PlayerScore: ${humanScore} ComputerScore: ${computerScore}`);
-} else if (computerScore > humanScore) {
-    console.log(`Game Over: Computer wins the game with a result of \n computerScore: ${computerScore} humanScore: ${humanScore}`);
-} else {
-    console.log(`Game Over: The Game is Draw with a result of \n PlayerScore: ${humanScore} ComputerScore: ${computerScore}`);
-}
+playGame();
